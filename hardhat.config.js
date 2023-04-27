528a2ccff7f46c760e45dadb401a381ec142aa10
Aro <aro.jormand@gmail.com>
2023-04-27 20:12:36 +0200

    swap token test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: "./.env.local" });
+
+const NEXT_PUBLIC_ALCHEMY_MAINNET_URL =
+  process.env.NEXT_PUBLIC_ALCHEMY_MAINNET_URL;
+
+console.log(NEXT_PUBLIC_ALCHEMY_MAINNET_URL);
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    hardhat: {
+      chainId: 1337, // for local development
+      forking: {
+        url: NEXT_PUBLIC_ALCHEMY_MAINNET_URL,
+      },
+    },
+  },
 };
